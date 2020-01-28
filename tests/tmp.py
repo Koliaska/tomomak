added 3rd 3d style and corrected plot units for detector geometry

tests/tmp.py
@@ -52,7 +52,7 @@ import tomomak.constraints.basic
 # The first step is to create coordinate system. We will consider 2D cartesian coordinates.
 # Let's create coordinate mesh. First axis will consist of 20 segments. Second - of 30 segments.
 # This means that solution will be described by the 20x30 array.
-axes = [cartesian.Axis1d(name="X", units="cm",  coordinates=(0.1, 0.3, 0.5, 0.7, 0.9, 1.1, 1.3, 1.5, 1.7, 1.9, 2.1, 2.5, 3,3.5, 4,4.5, 5, 7, 9,12)),
+axes = [cartesian.Axis1d(name="X", units="cm",  size=20, upper_limit=10),
         cartesian.Axis1d(name="Y", units="cm", size=20, upper_limit=10),
         cartesian.Axis1d(name="Z", units="cm", size=20, upper_limit=10)]
 mesh = mesh.Mesh(axes)
@@ -60,16 +60,9 @@ mod = model.Model(mesh=mesh)
 real_solution = objects2d.ellipse(mesh, (5,5),(3,3))
 noise = np.random.normal(0, 0.05, real_solution.shape)
 mod.solution = real_solution + noise
-
-from mayavi import mlab
-x, y, z = np.mgrid[-10:10:20j, -10:10:20j, -10:10:20j]
-s = np.sin(x*y*z)/(x*y*z)
-
-# mlab.pipeline.volume(mlab.pipeline.scalar_field(x,y,z,s), vmin=0, vmax=0.8)
-#
-# mlab.show()
 mod.detector_geometry = np.array([mod.solution, mod.solution + noise, mod.solution + noise * 2])
-obj, cb = mod.plot3d(data_type = 'detector_geometry', equal_norm=True, axes=True)
+# mod.plot2d(data_type = 'detector_geometry')
+obj, cb = mod.plot3d(data_type = 'detector_geometry', equal_norm=True, axes=True, style=1)
 mod.plot2d()
 
 

tomomak/mesh/mesh.py
@@ -133,7 +133,7 @@ class Mesh:
         if isinstance(index, int):
             index = [index]
         new_data = self._prepare_data(data, index, data_type)
-        plot = self._axes[index[0]].plot1d(new_data, data_type, *args, **kwargs)
+        plot = self._axes[index[0]].plot1d(new_data, self, data_type, *args, **kwargs)
         return plot
 
     def plot2d(self, data, index=0, data_type='solution', *args, **kwargs):
@@ -143,17 +143,17 @@ class Mesh:
         if len(index) == 1:
             try:
                 new_data = self._prepare_data(data, index[0], data_type)
-                plot = self._axes[index[0]].plot2d(new_data, *args, **kwargs)
+                plot = self._axes[index[0]].plot2d(new_data, self, data_type,  *args, **kwargs)
                 return plot
             except (NotImplementedError, TypeError):
                 index.append(index[0] + 1)
         # try to draw using 2 axes
         new_data = self._prepare_data(data, index, data_type)
         try:
-            plot = self._axes[index[0]].plot2d(new_data, self._axes[index[1]], data_type, *args, **kwargs)
+            plot = self._axes[index[0]].plot2d(new_data, self._axes[index[1]], self, data_type, *args, **kwargs)
         except NotImplementedError:
             new_data = new_data.transpose()
-            plot = self._axes[index[1]].plot2d(new_data, self._axes[index[0]], data_type, *args, **kwargs)
+            plot = self._axes[index[1]].plot2d(new_data, self._axes[index[0]], self, data_type, *args, **kwargs)
         return plot
 
     def plot3d(self, data, index=0, data_type='solution', *args, **kwargs):
@@ -163,19 +163,19 @@ class Mesh:
         if len(index) == 1:
             try:
                 new_data = self._prepare_data(data, index[0], data_type)
-                plot = self._axes[index[0]].plot3d(new_data, *args, **kwargs)
+                plot = self._axes[index[0]].plot3d(new_data, self, *args, **kwargs)
                 return plot
             except (NotImplementedError, TypeError):
                 index.append(index[0] + 1)
         # try to draw using 2 axes
         new_data = self._prepare_data(data, index, data_type)
         try:
-            plot = self._axes[index[0]].plot3d(new_data, self._axes[index[1]], data_type, *args, **kwargs)
+            plot = self._axes[index[0]].plot3d(new_data, self._axes[index[1]], self, data_type, *args, **kwargs)
             return plot
         except (NotImplementedError,  TypeError):
             try:
                 new_data = new_data.transpose()
-                plot = self._axes[index[1]].plot3d(new_data, self._axes[index[0]], data_type, *args, **kwargs)
+                plot = self._axes[index[1]].plot3d(new_data, self._axes[index[0]], self, data_type, *args, **kwargs)
                 return plot
             except (NotImplementedError, TypeError):
                 index.append(index[1] + 1)
@@ -188,7 +188,8 @@ class Mesh:
                 dat = np.array(new_data)
                 for i in range(3):
                     np.moveaxis(dat, p[i], i)
-                plot = new_ax[index[0]].plot3d(dat, new_ax[index[1]], new_ax[index[2]], data_type, *args, **kwargs)
+                plot = new_ax[index[0]].plot3d(dat, new_ax[index[1]], new_ax[index[2]],
+                                               self, data_type, *args, **kwargs)
                 return plot
             except NotImplementedError:
                 pass

tomomak/plots/plot2d.py
@@ -46,7 +46,7 @@ def colormesh2d(data, axis1, axis2, title='', fill_scheme='viridis', grid=False,
     return plot, ax, fig, cb
 
 
-def detector_colormesh2d(data, axis1, axis2, title='', fill_scheme='viridis',
+def detector_colormesh2d(data, axis1, axis2, title='', cb_title='', fill_scheme='viridis',
                          grid=False, equal_norm=False, *args, **kwargs):
     """Prepare bar plot for 2D detector data visualization with interactive elements.
 
@@ -56,11 +56,12 @@ def detector_colormesh2d(data, axis1, axis2, title='', fill_scheme='viridis',
         data (ndarray): 2D array of data.
         axis1 (axis): corresponding tomomak axis № 1.
         axis2 (axis): corresponding tomomak axis № 2.
-        title (str, optional): Plot title. default: ''.
-        fill_scheme (pyplot colormap, optional): pyplot colormap to be used in the plot. default: 'viridis'.
-        grid (bool, optional): if True, grid is shown. default: False.
+        title (str, optional): Plot title. Default: ''.
+        cb_title (str, optional) Colorbar title. Default: ''.
+        fill_scheme (pyplot colormap, optional): pyplot colormap to be used in the plot. Default: 'viridis'.
+        grid (bool, optional): if True, grid is shown. Default: False.
         equal_norm (bool, optional): If True,  all detectors will have same z axis.
-            If False, each detector has individual z axis. default:False
+            If False, each detector has individual z axis. Default:False
         *args, **kwargs: arguments will be passed to matplotlib.pyplot.pcolormesh
 
     Returns:
@@ -90,6 +91,7 @@ def detector_colormesh2d(data, axis1, axis2, title='', fill_scheme='viridis',
     if equal_norm:
         norm = [min(np.min(data), 0), np.max(data)]
     plot, ax, fig, cb = colormesh2d(data[0], axis1, axis2, title,  fill_scheme, grid, norm, *args, **kwargs)
+    cb.set_label(cb_title)
     callback = ColormeshSlice(data, ax, fig, cb, norm)
     b_next, b_prev = interactive.crete_prev_next_buttons(callback.next, callback.prev)
     return plot, ax, (b_next, b_prev)

tomomak/plots/plot3d.py
@@ -2,6 +2,7 @@ from mayavi import mlab
 import matplotlib.colors
 import matplotlib.pyplot as plt
 import numpy as np
+import warnings
 from traits.api import HasTraits, Range, Instance, on_trait_change
 from traitsui.api import View, Item, HGroup
 from tvtk.pyface.scene_editor import SceneEditor
@@ -38,30 +39,45 @@ def _build_contour3d(data, x, y, z, scene, title='', colormap='blue-red', limits
 
     # graph
     scene.background = (1, 1, 1)
-    obj = scene.mlab.contour3d(x, y, z, data, colormap=colormap,  opacity=0.05)
     min_ax_size = np.min(x.shape)
-    if style == 1:
-        obj.actor.property.line_width = 200/min_ax_size
-        obj.actor.property.representation = 'wireframe'
-        obj.actor.property.lighting = False
-        obj.contour.number_of_contours = 20
-    elif style == 2:
-        obj.actor.property.representation = 'points'
-        obj.actor.property.lighting = False
-        obj.actor.property.point_size = 200/min_ax_size
+    if style == 1 or style == 2:
+        obj = scene.mlab.contour3d(x, y, z, data, colormap=colormap, opacity=0.05)
+        if style == 1:
+            obj.actor.property.line_width = 200/min_ax_size
+            obj.actor.property.representation = 'wireframe'
+            obj.actor.property.lighting = False
+            obj.contour.number_of_contours = 20
+        elif style == 2:
+            obj.actor.property.representation = 'points'
+            obj.actor.property.lighting = False
+            obj.actor.property.point_size = 200/min_ax_size
+        if limits is None:
+            obj.contour.minimum_contour = np.amax(data) * 0.2
+            obj.contour.auto_update_range = False
+        else:
+            obj.contour.minimum_contour = limits[0]
+            obj.contour.maximum_contour = limits[1]
+    elif style == 3 or style == 33:
+        if limits is None:
+            max_val = np.amax(data)
+            min_val = max_val * 0.2
+        else:
+            min_val = limits[0]
+            max_val = limits[1]
+        if style == 33:  # special case to deal with Mayavi bug
+            obj = scene.mlab.pipeline.volume(mlab.pipeline.scalar_field(x, y, z, data))
+        else:
+            obj = scene.mlab.pipeline.volume(mlab.pipeline.scalar_field(x, y, z, data), vmin=min_val, vmax=max_val)
     else:
         raise AttributeError("Style {} is not supported".format(style))
-    if limits is None:
-        obj.contour.minimum_contour = np.amax(data) * 0.2
-        obj.contour.auto_update_range = False
-    else:
-        obj.contour.minimum_contour = limits[0]
-        obj.contour.maximum_contour = limits[1]
+
     # colorbar
     cb = scene.mlab.colorbar(title=title, orientation='vertical')
     cb.title_text_property.color = (0, 0, 0)
-    cb.label_text_property.line_offset = 2
+    cb.label_text_property.line_offset = 5
     cb.label_text_property.color = (0, 0, 0)
+    cb.scalar_bar.unconstrained_font_size = True
+    cb.title_text_property.font_size = 12
     # axes
     if axes:
         ax = scene.mlab.axes(xlabel=axes[0], ylabel=axes[1], zlabel=axes[2], nb_labels=5, color=(0, 0, 0))
@@ -106,7 +122,7 @@ def contour3d(data, x, y, z,  title='', colormap='blue-red', limits=None, style=
 
         # the layout of the dialog created
         view = View(Item('scene', editor=SceneEditor(scene_class=MayaviScene),
-                         height=400, width=500, show_label=False), resizable=True, title="Detectors")
+                         height=700, width=800, show_label=False), resizable=True, title="Detectors")
 
     visualization = Visualization()
     visualization.configure_traits()
@@ -137,6 +153,10 @@ def detector_contour3d(data, x, y, z,  title='', colormap='blue-red', limits=Non
           equal_norm(bool, optional): If True,  all detectors will have same z axis.
             If False, each detector has individual z axis. default:False
       """
+    # Currently there is a bug with Mayavi vmin and vmax limits in interactive volume render.
+    # So special rendering case is introduced.
+    if style == 3:
+        style = 33
     class Visualization(HasTraits):
         Detector = Range(1, data.shape[0], 1)
         scene = Instance(MlabSceneModel, ())
@@ -148,14 +168,18 @@ def detector_contour3d(data, x, y, z,  title='', colormap='blue-red', limits=Non
 
         @on_trait_change('scene.activated')
         def setup(self):
+
             self.plot = _build_contour3d(data[self.index], x, y, z,
                                          self.scene, title, colormap, limits, style, axes)
             if equal_norm:
-                max_d = np.max(data)
-                min_d = np.min(data)
-                self.plot.contour.minimum_contour = np.amax(data) * 0.2
-                self.plot.module_manager.scalar_lut_manager.use_default_range = False
-                self.plot.module_manager.scalar_lut_manager.data_range = [min_d, max_d]
+                if style != 33:
+                    max_d = np.max(data)
+                    min_d = np.min(data)
+                    self.plot.contour.minimum_contour = np.amax(data) * 0.2
+                    self.plot.module_manager.scalar_lut_manager.use_default_range = False
+                    self.plot.module_manager.scalar_lut_manager.data_range = [min_d, max_d]
+                else:
+                    warnings.warn("Equal norm is not supported for this style due to Mayavi bugs.")
 
         @on_trait_change('Detector')
         def update_plot(self):
@@ -164,7 +188,7 @@ def detector_contour3d(data, x, y, z,  title='', colormap='blue-red', limits=Non
 
         # the layout of the dialog created
         view = View(Item('scene', editor=SceneEditor(scene_class=MayaviScene),
-                         height=400, width=500, show_label=False),
+                         height=700, width=800, show_label=False),
                     HGroup('_', 'Detector'),
                     resizable=True, title="Detectors")
 