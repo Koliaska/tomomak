added toroidal cylindrical coordinate system + corrected broadcasting

tests/tmp.py
@@ -53,7 +53,7 @@ from tomomak import model
 from tomomak.solver import *
 from tomomak.test_objects import objects2d
 from tomomak.mesh import mesh
-from tomomak.mesh import cartesian, polar
+from tomomak.mesh import cartesian, polar, toroidal
 from tomomak.transform import rescale
 from tomomak.transform import pipeline
 from tomomak.detectors import detectors2d, signal
@@ -62,51 +62,83 @@ from tomomak.iterators import statistics
 import tomomak.constraints.basic
 import numpy as np
 
-# This is an example of a basic framework functionality.
-# You will learn how to use framework, steps you need to follow in order to get the solution.
-# More advanced features are described in advanced examples.
-
-# The first step is to create coordinate system. We will consider 2D cartesian coordinates.
-# Let's create coordinate mesh. First axis will consist of 20 segments. Second - of 30 segments.
-# This means that solution will be described by the 20x30 array.
-
-axes = [polar.Axis1d(name="phi", units="cm", size=20),
-        cartesian.Axis1d(name="R", units="cm", size=19, upper_limit=10),
-
-        cartesian.Axis1d(name="Z", units="cm", size=18, upper_limit=10),
-            ]
-mesh = Mesh(axes)
-mod = Model(mesh=mesh)
-
-# The next step - is to create a synthetic object.
-# Let's create 2D circle. Since our mesh is 3D it will be automatically broadcasted to 3rd dimension,
-# so we get a cylinder.
-real_solution = objects2d.ellipse(mesh, (0, 0), (3, 3))
-
-# Now let's add some noise to this object in order to simulate realistic distribution.
-noise = np.random.normal(0, 0.05, real_solution.shape)
-mod.solution = real_solution + noise
-mod.plot2d(cartesian_coordinates=True, )
-# Now it's time to do plotting
-mod.plot3d(style=1, axes=True, cartesian_coordinates=True)
-
-# You can play with the Mayavi pipeline (upper left corner) in order to get better-looking figure.
-# For example try to change Data minimum in contours tab of the isoSurface, or opacity in Actor tab.
-# There are other predefined styles which you can use as a starting position for plot generation.
-# Also let's add coordinate axes and change the style.
-
+# let's start by creating a 3D cartesian coordinate system.
+# Since 3D tomography is much slower than 2D, in the example we will use 10x10x10 grid,
+# but you can increase these values.
+axes = [toroidal.Axis1d(radius=20, name="theta", units="cm", size=20),
+        polar.Axis1d(name="phi", units="phi", size=11),
+        cartesian.Axis1d(name="Z", units="cm", size=10, upper_limit=10)]
+mesh = mesh.Mesh(axes)
+mod = model.Model(mesh=mesh)
+# geometry3d.show_cell(mesh, cell_index=(1, 1, 1))
+# Now let's create a test object. It will be a sphere-like source with 1/R^2 density.
+# Note that several object types are supported, including arbitrary objects, defined by vertices and faces,
+# however calculation of the most objects intersection with each grid cell will take significant time.
+# This is the price to pay for the versatility of the framework.
+real_solution = objects2d.ellipse(mesh, (0, 0), (4, 9), index=(1, 2))
+# Let's plot the data in different ways.
+mod.solution = real_solution
+mod.plot2d(index=(1,2), cartesian_coordinates=True)
+mod.plot3d(axes=True, style=3, cartesian_coordinates=True)
 # Open Mayavi pipeline -> Volumes -> CTF and try to change alpha channel curve (opacity) -> press update CTF.
 # This way, for example, you can hide the outer layers to look inside the object.
+mod.plot3d(axes=True, style=2)
+# Open Mayavi pipeline -> IsoSurface -> Actor and try to change Point size or opacity.
+
+# Ok, now let's add some detectors
+mod.solution = None
+# We will use an array of hermetic detectors.
+# Note that several object types are supported,
+# however calculation of the most detector line of sights intersection with each grid cell will take significant time.
+# The good news is that you need to do these calculations only once for a given geometry.
+# After that it is recommended to save calculation results, for example using >> mod.save("3dtomography.tmm")
+# There are ways to accelerate the calculations using multiprocess (see GPU and CPU acceleration example)
+# for example with detector_array function. Also detector line of sights may be represented as zero-width rays
+# (line_detector function with parameter calc_volume = False), which is much faster, but usually not recommended.
+mod.detector_geometry = detectors3d.four_pi_detector_array(mesh, focus_point=(0,0, 0),
+                                                           radius=45, theta_num=20, phi_num=20)
+
+mod.plot3d(axes=True, style=3, data_type='detector_geometry', cartesian_coordinates=True)
+# You can add other detectors - just uncomment the line and append the result  using mod.add_detector(det)
+# det = [detectors3d.aperture_detector(mesh, [(4, -5, 4), (6, -5, 4), (4, -5, 6), (6, -5, 6)],
+#                                      [(4, 0, 4), (6, 0, 4), (4, 0, 6), (6, 0, 6)])]
+# det = [detectors3d.aperture_detector(mesh, [(4, -5, 4), (6, -5, 4), (4, -5, 6), (6, -5, 6)],
+#                                                        [(4, 0, 4), (6, 0, 4), (4, 0, 6), (6, 0, 6)])]
+# ver = np.array([[0, 0, 0], [0, 0, 10], [0, 10, 0], [10, 0, 0]])
+# det = [detectors3d.custom_detector(mesh, ver, radius_dependence=False)]
+# det = [detectors3d.line_detector(mesh, (5, 5, 12), (10, 2, 0), 1, calc_volume=True)]
+# det = [detectors3d.cone_detector(mesh, (5, 5, 20), (5, 5, 0), 0.3)]
+# det = [detectors3d.line_detector(mesh, (5, 5, 12), (10, 2, 0), None, calc_volume=False)]
+# det = detectors3d.fan_detector(mesh, (-5, 5, 5), [[5, 2, 6], [5, 7, 6], [5, 2, 4], [5, 7, 4]],
+#                                number=[4, 4], divergence=0.3)
+det_signal = signal.get_signal(real_solution, mod.detector_geometry)
+mod.detector_signal = det_signal
+# Now let's find the solution
+solver = Solver()
+solver.statistics = [statistics.RN(), statistics.RMS()]
+solver.real_solution = real_solution
+solver.iterator = ml.ML()
+steps = 1500
+solver.solve(mod, steps=steps)
+mod.save("tor.tmm")
+# Now we can see the solution.
+mod.plot3d(axes=True, style=3, cartesian_coordinates=True)
+mod.plot2d(index=(1,2), cartesian_coordinates=True)
+
+solver.plot_statistics()
+# Of course, the result we got is not ideal due to limitations of our synthetic experiment.
+# Luckily there are ways to improve our calculations, described in the more advanced examples.
 
 
-# You can also plot detector geometry this way.
-# Let's create test geometry. You can switch between detectors with the bottom slider.
-# In this example we will use same norm for all detectors.
-mod.detector_geometry = np.array([mod.solution, mod.solution + noise, mod.solution + noise * 2])
-mod.plot3d(data_type='detector_geometry', equal_norm=True, cartesian_coordinates=True)
-# And that's how you deal with basic 3D plotting. for later usage. I
+axes = [toroidal.Axis1d(radius = 10, name="theta", units="rad", size=7),
+        polar.Axis1d(name="phi", units="rad", size=12),
+        cartesian.Axis1d(name="R", units="cm", size=8, upper_limit=9)]
 
+mesh = mesh.Mesh(axes)
+vertices, faces = axes[0].cell_edges3d_cartesian(axes[1], axes[2])
+geometry3d.show_cell(mesh, cell_index=(3, 3, 3))
 
+#
 # And that's it. You get solution, which is, of course, not perfect,
 # but world is not easy when you work with the limited data.
 # There are number of ways to improve your solution, which will be described in other examples.

tomomak/mesh/abstract_axes.py
@@ -368,6 +368,7 @@ class Abstract1dAxis(AbstractAxis):
                 x_grid, y_grid, z_grid, new_data = \
                     util.geometry3d.make_regular(data, x_grid, y_grid, z_grid, interp_size)
                 new_data = np.nan_to_num(new_data)
+                new_data = np.clip(new_data, np.amin(data), np.amax(data))
             else:
                 new_data = data
             # plot
@@ -392,10 +393,12 @@ class Abstract1dAxis(AbstractAxis):
                 for i, d in enumerate(data):
                     x_grid, y_grid, z_grid, new_data[i] \
                         = util.geometry3d.make_regular(d, x_grid_n, y_grid_n, z_grid_n, interp_size)
+                    new_data[i] = np.nan_to_num(new_data[i])
+                    new_data[i] = np.clip(new_data[i], np.amin(data[i]), np.amax(data[i]))
                     print('\r', end='')
                     print("...", str((i+1) * 100 // data.shape[0]) + "% complete", end='')
                 print('\r \r', end='')
-                new_data = np.nan_to_num(new_data)
+
             else:
                 new_data = data
             plot3d.detector_contour3d(new_data, x_grid, y_grid, z_grid,
@@ -457,7 +460,7 @@ class Abstract2dAxis(AbstractAxis):
         """
 
     @abstractmethod
-    def cell_edges2d(self):
+    def cell_edges2d_cartesian(self):
         """
 
         Args:
@@ -467,7 +470,7 @@ class Abstract2dAxis(AbstractAxis):
         """
 
     @abstractmethod
-    def cell_edges3d(self, axis2):
+    def cell_edges3d_cartesian(self, axis2):
         """
 
         Args:
@@ -491,7 +494,7 @@ class Abstract3dAxis(AbstractAxis):
         """
 
     @abstractmethod
-    def cell_edges3d(self):
+    def cell_edges3d_cartesian(self):
         """
 
         Args:

tomomak/mesh/polar.py
@@ -7,7 +7,11 @@ from tomomak import util
 
 
 class Axis1d(abstract_axes.Abstract1dAxis):
-    RESOLUTION2D = 20
+    """
+    polar + cartesian -> 2d polar
+    polar + cartesian + cartesian -> 3d cylindrical
+    """
+    RESOLUTION2D = 10
 
     def __init__(self, coordinates=None, edges=None, lower_limit=0, upper_limit=2*np.pi, size=None, name="", units=""):
         super().__init__(coordinates, edges, lower_limit, upper_limit, size, name, units)
@@ -46,14 +50,15 @@ class Axis1d(abstract_axes.Abstract1dAxis):
             raise TypeError("Cell edges with such combination of axes are not supported.")
 
     def cell_edges3d_cartesian(self, axis2, axis3):
-        # #Cylindrical coordinates
+        # Cylindrical coordinates
         if type(axis2) is cartesian.Axis1d and type(axis3) is cartesian.Axis1d:
             shape = (self.size, axis2.size, axis3.size)
             vertices = np.zeros(shape).tolist()
             faces = np.zeros(shape).tolist()
             edge3 = axis3.cell_edges
             edges2d = self.cell_edges2d_cartesian(axis2)
-            # precalculate faces for 2 cases - 1) standard, 2) - center of the mesh
+            # Precalculate faces for 2 cases - 1) standard, 2) - center of the mesh
+            # 1) standart
             face = list()
             # left and right faces
             face.append((0, 1, self.RESOLUTION2D * 2 + 3, self.RESOLUTION2D * 2 + 2))
@@ -77,6 +82,7 @@ class Axis1d(abstract_axes.Abstract1dAxis):
                 # bottom
                 face.append((self.RESOLUTION2D * 2 - i, self.RESOLUTION2D * 2 - i + 1,
                              self.RESOLUTION2D * 4 - i + 3, self.RESOLUTION2D * 4 - i + 2))
+            # 2) - center of the mesh
             faces_center = list()
             # left and right faces as triangles
             faces_center.append((0, 1, self.RESOLUTION2D + 2))
@@ -103,7 +109,7 @@ class Axis1d(abstract_axes.Abstract1dAxis):
                             faces[i][j][k] = face
                         else:
                             faces[i][j][k] = faces_center
-            return vertices, faces
+            return np.array(vertices, dtype=object), np.array(faces, dtype=object)
         else:
             raise TypeError("Cell edges with such combination of axes are not supported.")
 

tomomak/util/array_routines.py
@@ -41,21 +41,20 @@ def broadcast_object(ar, index, shape):
         # Moving current axes to the end in order to prepare shape for numpy broadcast
         shape = list(shape)
         index = list(index)
-        index.reverse()
         current_shape = []
         for i, ind in enumerate(index):
-            val = shape.pop(ind)
+            val = shape[ind]
             current_shape.append(val)
-        current_shape.reverse()
+        sorted_index = np.sort(index)[::-1]
+        for i, ind in enumerate(sorted_index):
+            del shape[ind]
         for ind in current_shape:
             shape.append(ind)
         # broadcasting
         ar = np.broadcast_to(ar, shape)
-        index.reverse()
         # making correct axes order
         ind_len = len(index)
-        for i, ind in enumerate(index):
-            ar = np.moveaxis(ar, -ind_len + i, ind)
+        ar = np.moveaxis(ar, range(-ind_len, -ind_len + len(index)), index)
         return ar
 
 

tomomak/util/geometry2d.py
@@ -10,8 +10,8 @@ def intersection_2d(mesh, points, index=(0, 1), calc_area=True):
     Each value in the array corresponds to the intersection area of the given cell and the polygon.
     If there are more than 2 dimension in model, broadcasting to other dimensions is performed.
     If broadcasting is not needed private method _polygon may be used.
-    Only axes which implements cell_edges2d method are supported.
-    cell_edges2d method should  accept second axe and return 2d list of ordered sequence of point tuples for two 1d axes
+    Only axes which implements cell_edges2d_cartesian method are supported.
+    cell_edges2d_cartesian method should  accept second axe and return 2d list of ordered sequence of point tuples for two 1d axes
     or 1d list of ordered sequence of point tuples for one 2d axis.
     Each point tuple represents cell borders in the 2D cartesian coordinates.
     E.g. borders of the cell of two cartesian axes with edges (0,7) and (0,5)
@@ -30,7 +30,7 @@ def intersection_2d(mesh, points, index=(0, 1), calc_area=True):
         ndarray: 1D or 2D numpy array, representing polygon on the given mesh.
 
     Raises:
-        TypeError if no combination of axes supports the cell_edges2d() method or all axes dimensions are > 2.
+        TypeError if no combination of axes supports the cell_edges2d_cartesian() method or all axes dimensions are > 2.
     """
     if isinstance(index, int):
         index = [index]
@@ -53,7 +53,7 @@ def intersection_2d(mesh, points, index=(0, 1), calc_area=True):
                         res[i] = cell.area
             return res
         except (TypeError, AttributeError) as e:
-            raise type(e)(e.message + "Custom axis should implement cell_edges2d method. "
+            raise type(e)(e.message + "Custom axis should implement cell_edges2d_cartesian method. "
                                       "This method returns 2d list of ordered sequence of point tuples."
                                       " See docstring for more information.")
     # If axes are 1D
@@ -67,7 +67,7 @@ def intersection_2d(mesh, points, index=(0, 1), calc_area=True):
                 cells = np.array(mesh.axes[i2].cell_edges2d_cartesian(mesh.axes[i1]), dtype=object)
                 cells =np.transpose(cells)
             except (NotImplementedError, TypeError) as e:
-                raise type(e)("Custom axis should implement cell_edges2d method. "
+                raise type(e)("Custom axis should implement cell_edges2d_cartesian method. "
                               "This method returns 2d list of ordered sequence of point tuples."
                               " See docstring for more information.")
     else:

tomomak/util/geometry3d.py
@@ -12,7 +12,7 @@ def get_trimesh_grid(mesh, index=(0, 1, 2)):
     """Get array of trimesh objects (3D meshes) corresponding to each grid cell.
 
     Helper function. Trimesh objects are used for complex 3d calculations.
-    Only axes which implements cell_edges3d method are supported (see abstract_axes.py).
+    Only axes which implements cell_edges3d_cartesian method are supported (see abstract_axes.py).
     Trimesh module is used for the calculation.
 
     Args:
@@ -23,7 +23,7 @@ def get_trimesh_grid(mesh, index=(0, 1, 2)):
         list: 1D/2D or 3D list (depending on used tomomak mesh), containing trimesh representation of each cell.
 
     Raises:
-        TypeError if no combination of axes supports the cell_edges3d() method or all axes dimensions are > 3.
+        TypeError if no combination of axes supports the cell_edges3d_cartesian() method or all axes dimensions are > 3.
     """
     if isinstance(index, int):
         index = [index]
@@ -39,11 +39,11 @@ def get_trimesh_grid(mesh, index=(0, 1, 2)):
                     cell = trimesh.Trimesh(vertices=vertices[i], faces=faces[i])
                 trimesh_list[i] = cell
         except (TypeError, AttributeError) as e:
-            raise type(e)(e.message + "Custom axis should implement cell_edges3d method. "
+            raise type(e)(e.message + "Custom axis should implement cell_edges3d_cartesian method. "
                                       " See docstring for more information.")
     # If 1st axis is 2D
     elif mesh.axes[index[0]].dimension == 2 or mesh.axes[index[1]].dimension == 2:
-        (vertices, faces) = mesh.axes_method2d(index, "cell_edges3d")
+        (vertices, faces) = mesh.axes_method2d(index, "cell_edges3d_cartesian")
         vertices = vertices.tolist()
         faces = faces.tolist()
         shape = (mesh.axes[index[0]].size, mesh.axes[index[1]].size)
@@ -57,7 +57,7 @@ def get_trimesh_grid(mesh, index=(0, 1, 2)):
                 trimesh_list[i][j] = cell
     # If axes are 1D
     elif mesh.axes[index[0]].dimension == mesh.axes[index[1]].dimension == mesh.axes[index[2]].dimension == 1:
-        (vertices, faces) = mesh.axes_method3d(index, "cell_edges3d")
+        (vertices, faces) = mesh.axes_method3d(index, "cell_edges3d_cartesian")
         vertices = vertices.tolist()
         faces = faces.tolist()
         shape = (mesh.axes[index[0]].size, mesh.axes[index[1]].size, mesh.axes[index[2]].size)
@@ -381,7 +381,7 @@ def mesh_center(mesh, index):
     Returns:
         list: (x, y, z) coordinates of the mesh center.
     """
-    vertices, _ = mesh.axes_method3d(index, 'cell_edges3d')
+    vertices, _ = mesh.axes_method3d(index, 'cell_edges3d_cartesian')
     min_v = np.array(vertices[0, 0, 0][0])
     max_v = np.array(vertices[0, 0, 0][0])
     for i in vertices: